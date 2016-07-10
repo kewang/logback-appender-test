970b98bb62ab491903cb3b45274ef1025a03180c
kewang <cpckewang@gmail.com>
2016-07-10 21:07:28 +0800

    Add web-logback

--- src/main/java/tw/kewang/App.java
+++ src/main/java/tw/kewang/App.java
@@ -7,6 +7,14 @@ public class App {
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        LOG.info("Hello World!");
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                LOG.error("Caught Exception: ", e);
+            }
+
+            LOG.info("Hello World!");
+        }
     }
 }
